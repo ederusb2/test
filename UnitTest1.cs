2eb0112583bfa88c3da0fba99ac1308a58c528cb
ederusb2 <Evgeny.Derevyago@terumobct.com>
2026-03-10 15:14:41 +0300

    Init project

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace TestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        const string message = "Hello, World!";
+        message.Should().BeOfType<string>("Message has not correct type");
+    }
+}
